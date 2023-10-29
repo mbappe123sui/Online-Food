12c0e3e169b5a9295ccadc67f59dad4a928a8acb
mbappe123sui <rilwanabdulsalamig@gmail.com>
2023-10-29 17:20:08 +0100

    vscode

---
+++ login.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Login</title>
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css">
+
+  <link rel='stylesheet prefetch' href='https://fonts.googleapis.com/css?family=Roboto:400,100,300,500,700,900|RobotDraft:400,100,300,500,700,900'>
+<link rel='stylesheet prefetch' href='htpps://maxcdn.bootstrap.com/font-awesome/4.3.0/css/font-awesome.min.css'>
+
+       <link rel="stylesheet" href="css/login.css">
+
+    <style type="text/css">
+    #buttn{
+        color:#fff;
+        background-color: #5c4ac7;
+    }   
+    </style>
+
+
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit-no">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/animsition.min.css">
+    <link rel="stylesheet" href="css/animate.css">
+    <link rel="stylesheet" href="css/style.css">
+
+</head>
+
+<body>
+<header id="header" class="header-scroll top-header headrom">
+            <nav class="navbar navbar-dark">
+                <div class="container">
+                    <button class="navbar-toggler hidden-lg-up" type="button" data-toggle="collapse" data-target="#mainNavbarCollapse">&#9776;</button>
+                    <a class="navbar-brand" href="index.php"> <img class="img-rounded" src="images/icn.png" alt=""> </a>
+                    <div class="collapse navbar-toggleable-md  float-lg-right" id="mainNavbarCollapse">
+                        <ul class="nav navbar-nav">
+                            <li class="nav-item"> <a class="nav-link active" href="index.php">Home <span class="sr-only">(current)</span></a> </li>
+                            <li class="nav-item"> <a class="nav-link active" href="restaurants.php">Restaurants <span class="sr-only"></span></a> </li>
+                            
+                           
+							<?php
+						if(empty($_SESSION["user_id"])) // if user is not login
+							{
+								echo '<li class="nav-item"><a href="login.php" class="nav-link active">Login</a> </li>
+							  <li class="nav-item"><a href="registration.php" class="nav-link active">Register</a> </li>';
+							}
+						else
+							{
+
+									
+									echo  '<li class="nav-item"><a href="your_orders.php" class="nav-link active">My Orders</a> </li>';
+									echo  '<li class="nav-item"><a href="logout.php" class="nav-link active">Logout</a> </li>';
+							}
+
+						?>
+							 
+                        </ul>
+						 
+                    </div>
+                </div>
+            </nav>
+
+        </header>    
+</body>
+</html>

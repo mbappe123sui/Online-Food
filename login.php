df81580babf7bdd4741fcc2a27a971c57679fb75
mbappe123sui <rilwanabdulsalamig@gmail.com>
2024-02-11 17:11:20 +0100

    my second push

--- login.php
+++ login.php
@@ -63,6 +63,110 @@
                 </div>
             </nav>
 
-        </header>    
+        </header>   
+        <div style=" background-image: url('images/img/pimg.jpg');">
+
+<?php
+include("connection/connect.php"); 
+error_reporting(0); 
+session_start(); 
+if(isset($_POST['submit']))  
+{
+	$username = $_POST['username'];  
+	$password = $_POST['password'];
+	
+	if(!empty($_POST["submit"]))   
+     {
+	$loginquery ="SELECT * FROM users WHERE username='$username' && password='".md5($password)."'"; //selecting matching records
+	$result=mysqli_query($db, $loginquery); //executing
+	$row=mysqli_fetch_array($result);
+	
+	                        if(is_array($row)) 
+								{
+                                    	$_SESSION["user_id"] = $row['u_id']; 
+										 header("refresh:1;url=index.php"); 
+	                            } 
+							else
+							    {
+                                      	$message = "Invalid Username or Password!"; 
+                                }
+	 }
+	
+	
+}
+?>
+  
+
+<div class="pen-title">
+  <
+</div>
+
+<div class="module form-module">
+  <div class="toggle">
+   
+  </div>
+  <div class="form">
+    <h2>Login to your account</h2>
+	  <span style="color:red;"><?php echo $message; ?></span> 
+   <span style="color:green;"><?php echo $success; ?></span>
+    <form action="" method="post">
+      <input type="text" placeholder="Username"  name="username"/>
+      <input type="password" placeholder="Password" name="password"/>
+      <input type="submit" id="buttn" name="submit" value="Login" />
+    </form>
+  </div>
+  
+  <div class="cta">Not registered?<a href="registration.php" style="color:#5c4ac7;"> Create an account</a></div>
+</div>
+  <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+
+  
+
+   
+  <div class="container-fluid pt-3">
+	<p></p>
+  </div>
+
+
+   
+        <footer class="footer">
+            <div class="container">
+
+             
+                <div class="bottom-footer">
+                    <div class="row">
+                        <div class="col-xs-12 col-sm-3 payment-options color-gray">
+                            <h5>Payment Options</h5>
+                            <ul>
+                                <li>
+                                    <a href="#"> <img src="images/paypal.png" alt="Paypal"> </a>
+                                </li>
+                                <li>
+                                    <a href="#"> <img src="images/mastercard.png" alt="Mastercard"> </a>
+                                </li>
+                                <li>
+                                    <a href="#"> <img src="images/maestro.png" alt="Maestro"> </a>
+                                </li>
+                                <li>
+                                    <a href="#"> <img src="images/stripe.png" alt="Stripe"> </a>
+                                </li>
+                                <li>
+                                    <a href="#"> <img src="images/bitcoin.png" alt="Bitcoin"> </a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="col-xs-12 col-sm-4 address color-gray">
+                            <h5>Address</h5>
+                            <p>1086 Stockert Hollow Road, Seattle</p>
+                                    <h5>Phone: 75696969855</a></h5> </div>
+                        <div class="col-xs-12 col-sm-5 additional-info color-gray">
+                            <h5>Addition informations</h5>
+                           <p>Join thousands of other restaurants who benefit from having partnered with us.</p>
+                        </div>
+                    </div>
+                </div>
+            
+            </div>
+        </footer> 
 </body>
 </html>
